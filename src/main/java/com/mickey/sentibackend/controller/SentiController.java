88ac1da912e11f071f1d7462d853454bc0016935
liuyu <201250141@smail.nju.edu.cn>
2023-03-29 21:42:01 +0800

    feat: 完成分析文本请求, 添加了允许跨域注解

--- src/main/java/com/mickey/sentibackend/controller/SentiController.java
+++ src/main/java/com/mickey/sentibackend/controller/SentiController.java
@@ -1,7 +1,11 @@
 package com.mickey.sentibackend.controller;
 
 import com.mickey.sentibackend.entity.Result;
+import com.mickey.sentibackend.exception.SentiException;
+import com.mickey.sentibackend.service.SentiService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +13,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.wlv.sentistrength.SentiStrength;
 
-/**
- * @author 刘屿
- */
 @RestController
 @RequestMapping("/api/v1")
 @Slf4j
+@CrossOrigin
 public class SentiController {
+
+    private final SentiService sentiService;
+
+    @Autowired
+    public SentiController(SentiService sentiService) {
+        this.sentiService = sentiService;
+    }
+
+    /**
+     * 用于处理解析文本的请求
+     * @param text 带分析文本
+     * @param type 分析模式
+     * @param explain 是否需要解释
+     * @return 分析结果
+     */
+    @PostMapping("/text")
+    public Result<String> analyzeText(@RequestParam("text") String text,
+                                      @RequestParam("type") String type,
+                                      @RequestParam("explain") Boolean explain) {
+        String result = sentiService.analyzeText(text, type, explain);
+        return Result.buildSuccess(result);
+    }
 
     /**
      * 该方法仅用于测试连接和jar包的使用
---
+++ src/main/java/com/mickey/sentibackend/service/SentiService.java
@@ -0,0 +1,71 @@
+package com.mickey.sentibackend.service;
+
+import com.mickey.sentibackend.exception.SentiException;
+import org.springframework.stereotype.Service;
+import uk.ac.wlv.sentistrength.SentiStrength;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SentiService {
+
+    /**
+     * 方法用于分析文本
+     * @param text 带分析文本
+     * @param type 分析模式
+     * @param explain 是否需要解释
+     * @return 分析结果
+     */
+    public String analyzeText(String text, String type, Boolean explain) {
+        SentiStrength sentiStrength = new SentiStrength();
+        ArrayList<String> paramList = new ArrayList<>();
+        // 添加字典
+        paramList.add("sentidata");
+        paramList.add("./src/main/resources/SentStrength_Data/");
+//        paramList.add("/home/lighthouse/SentStrength_Data/");
+        // 添加模式
+        if (!addTypeParam(paramList, type)) {
+            throw new SentiException(1, "分析模式参数错误");
+        }
+        // 添加解释
+        if (explain) {
+            paramList.add("explain");
+        }
+        // 列表转数组初始化
+        String[] initArray = paramList.toArray(new String[0]);
+        sentiStrength.initialise(initArray);
+
+        return sentiStrength.computeSentimentScores(text);
+    }
+
+
+    /**
+     * 方法用于向参数列表添加分析模式
+     * @param paramList 参数列表
+     * @param type 模式字符串
+     * @return 模式是否合法
+     */
+    private boolean addTypeParam(List<String> paramList, String type) {
+        boolean result = false;
+        switch (type) {
+            case "trinary":
+                paramList.add("trinary");
+                result = true;
+                break;
+            case "binary":
+                paramList.add("binary");
+                result = true;
+                break;
+            case "scale":
+                paramList.add("scale");
+                result = true;
+                break;
+            case "default":
+                result = true;
+                break;
+            default:
+        }
+        return result;
+    }
+}
